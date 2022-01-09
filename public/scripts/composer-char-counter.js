dd87a2dce83c7935420b8c49e5f2a91b1cc848af
Steven Ngov <gl.steven.ngov@gmail.com>
2022-01-09 06:41:06 +0000

    Implement tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+/* eslint-env es6 */
+/* eslint-disable */
+
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+    console.log(this);
+    let count = $(".footer").find(".counter");
+    let textCount = $(this).val().length;
+    let countUpdate = $(count.val(140 - textCount));
+
+    if (Number(countUpdate.val()) < 0) {
+      $(count).css("color", "red");
+    } else {
+      $(count).css("color", "#545149");
+    };
+
+  });
+});
